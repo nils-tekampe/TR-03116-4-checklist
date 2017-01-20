f07c44575c38536f836f54c53bc531562d39fa7e
Nils Tekampe <n.tekampe@tuvit.de>
2017-01-20 23:42:03 +0100

    working

--- certificates.py
+++ certificates.py
@@ -4,11 +4,21 @@
 import socket, ssl
 import logging, logging.config, os, pem
 import subprocess
-from cryptography import x509
+from cryptography  import x509
 from cryptography.hazmat.backends import default_backend
 from tls_includes import cipher_suites
 import colorlog
 from helper import which, logger
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
+from cryptography.hazmat.primitives.asymmetric.dsa import DSAPublicKey
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePublicKey
+from cryptography.hazmat.backends.openssl.rsa import _RSAPublicKey
+from cryptography.x509 import Certificate
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurve
+import cryptography.x509
+from cryptography.x509.oid import ExtensionOID
+
 
 
 def read_certificates(hostname,port):
@@ -23,54 +33,136 @@ def read_certificates(hostname,port):
         (out, err) = proc.communicate()
 
         certs = pem.parse(out)
-
         logger.info(str(len(certs)) +" Certificates have been obtained.")
 
-        return certs
+        x509_certs = [load_pem_x509_certificate(str(x).encode('ascii','ignore'),default_backend()) for x in certs]
+
+        return x509_certs
+
 
     except Exception as err:
         print err
 
 
+def check_certificate_key(cert):
+    if (type(cert.public_key()) is _RSAPublicKey):
+        logger.info("This certificate has an RSA key")
+        if cert.public_key().key_size >= 2048:
+            logger.info("The key size is equal to or greater than 2048.")
+        else:
+            logger.error("The key size is smaller than 2048. This should not be the case")
 
-        for entry in certs:
-            cert = x509.load_pem_x509_certificate(str(entry).encode('ascii','ignore'), default_backend())
-            logger.info("Now checking certificate with serial: "+str(cert.serial_number))
-            logger.info("Here come the attributes of the subject of the certificate:")
-            for attribute in cert.subject:
-                logger.info(attribute)
+        # logger.info.("The key size is: "+str(cert.public_key().key_size))
+    if (type(cert.public_key())==DSAPublicKey):
+        logger.error("This certificate has an DSA key. This should not be the case")
+        #TODO: Der Fall muss noch getestet werden. Die genaue Bezeichnung des Types des public_key ist vermutlich anders
+    if (type(cert.public_key())==EllipticCurvePublicKey):
+        logger.info("This certificate has an EllipticCurvePublicKey key")
+        print cert.public_key().curve.name
+        #TODO: Checken, dass der name der Kurve denen in der Checkliste entspricht
+        #TODO: Der Fall muss noch getestet werden. Die genaue Bezeichnung des Types des public_key ist vermutlich anders
 
-            logger.info("The signature algorithm of the certificate is: "+str(cert.signature_algorithm_oid))
-
-            # if (type(cert.public_key())==cryptography.hazmat.backends.openssl.rsa._RSAPublicKey):
-            #     logger.info("This certificate has an RSA key")
-                # logger.info.("The key size is: "+str(cert.public_key().key_size)
-            # if (type(cert.public_key())==cryptography.hazmat.primitives.asymmetric.dsa.DSAPublicKey):
-            #     logger.error("This certificate has an DSA key. This should not be the case")
-            # if (type(cert.public_key())==cryptography.hazmat.primitives.asymmetric.ec.EllipticCurvePublicKey):
-            #     logger.info("This certificate has an EllipticCurvePublicKey key")
+def check_signature_algorithm(cert):
+    logger.warning("The signature algorithm of the certificate is: "+str(cert.signature_algorithm_oid._name))
+    logger.warning("The corresponding OID is: "+str(cert.signature_algorithm_oid.dotted_string))
+    logger.warning("Please check with the checklist")
 
 
-            logger.info("The certificate contains the following extensions:")
-            for extension in cert.extensions:
-                logger.info(extension.oid)
+def check_for_wildcards(cert):
+    print cert.subject._attributes
 
-            keyUsage=cert.extensions.get_extension_for_class(x509.KeyUsage)
-            # print keyUsage
-            # print type(keyUsage)
-            logger.warning("The keyUsage extension contains:")
-            logger.warning("digital_signature:"+str(keyUsage.value.digital_signature))
-            logger.warning("key_cert_sign:"+str(keyUsage.value.key_cert_sign))
-            logger.warning("crl_sign:"+str(keyUsage.value.crl_sign))
+    print type(cert.subject._attributes[1])
 
-            # extendedKeyUsage=cert.extensions.get_extension_for_class(x509.ExtendedKeyUsage)
-            # logger.warning("The extendedKeyUsage extension contains:")
-            # logger.warning(extendedKeyUsage)
+    #TODO: Muss noch fertiggestellt werden
+    #TODO: auch die Extension fpr Alternative Names checken
 
-            # print cert.serial_number
-            # print cert.subject
-            # print cert.issuer
-            # print cert.signature_hash_algorithm
-            # print cert.signature_algorithm_oid
-            # print cert.public_key()
-            # print cert.extensions
+
+def check_cert_for_crl(cert):
+    try:
+        crl_extension=cert.extensions.get_extension_for_class(x509.CRLDistributionPoints)
+        logger.info("The certificate has a CRLDistributionPoint Extension")
+
+    except Exception as err:
+        print err
+        #TODO: wenn es die Extension nicht gibt, tritt vermutlich ein Fehler auf, den man hier behandeln sollte
+
+def check_cert_for_aia(cert):
+    try:
+        crl_extension=cert.extensions.get_extension_for_class(x509.AuthorityInformationAccess)
+        logger.info("The certificate has a AuthorityInformationAccess Extension")
+
+    except Exception as err:
+        print err
+        #TODO: wenn es die Extension nicht gibt, tritt vermutlich ein Fehler auf, den man hier behandeln sollte
+
+def check_cert_for_revocation(cert):
+    try:
+        crl_extension=cert.extensions.get_extension_for_class(x509.CRLDistributionPoints)
+        logger.info("The certificate has a CRLDistributionPoint Extension")
+#TODO: CRL auswerten
+
+    except Exception as err:
+        print err
+        #TODO: wenn es die Extension nicht gibt, tritt vermutlich ein Fehler auf, den man hier behandeln sollte
+
+def check_cert_for_keyusage(cert):
+    try:
+        keyusage_extension=cert.extensions.get_extension_for_class(x509.KeyUsage)
+        logger.info("The certificate has a KeyUsage Extension with the following settings")
+        logger.warning("digital_signature: "+ str(keyusage_extension.value.digital_signature))
+        logger.warning("key_cert_sign: "+ str(keyusage_extension.value.key_cert_sign))
+        logger.warning("crl_sign: "+ str(keyusage_extension.value.crl_sign))
+
+        #TODO: Man könnte die Werte auch gleich prüfen.
+
+    except Exception as err:
+        print err
+        #TODO: wenn es die Extension nicht gibt, tritt vermutlich ein Fehler auf, den man hier behandeln sollte
+
+def check_cert_for_extended_keyusage(cert):
+    try:
+        keyusage_extension=cert.extensions.get_extension_for_class(x509.ExtendedKeyUsage)
+        logger.info("The certificate has a ExtendedKeyUsage Extension with the following settings")
+        # logger.warning("serverAuth: "+ str(keyusage_extension.value.SERVER_AUTH))
+
+        print dict(keyusage_extension)
+
+
+        #TODO: Ist das der richtige Wert?
+    except Exception as err:
+        print err
+        #TODO: wenn es die Extension nicht gibt, tritt vermutlich ein Fehler auf, den man hier behandeln sollte
+
+
+        # for entry in certs:
+        #     cert = x509.load_pem_x509_certificate(str(entry).encode('ascii','ignore'), default_backend())
+        #     logger.info("Now checking certificate with serial: "+str(cert.serial_number))
+        #     logger.info("Here come the attributes of the subject of the certificate:")
+        #     for attribute in cert.subject:
+        #         logger.info(attribute)
+        #
+        #
+        #
+        #     logger.info("The certificate contains the following extensions:")
+        #     for extension in cert.extensions:
+        #         logger.info(extension.oid)
+        #
+        #     keyUsage=cert.extensions.get_extension_for_class(x509.KeyUsage)
+        #     # print keyUsage
+        #     # print type(keyUsage)
+        #     logger.warning("The keyUsage extension contains:")
+        #     logger.warning("digital_signature:"+str(keyUsage.value.digital_signature))
+        #     logger.warning("key_cert_sign:"+str(keyUsage.value.key_cert_sign))
+        #     logger.warning("crl_sign:"+str(keyUsage.value.crl_sign))
+        #
+        #     # extendedKeyUsage=cert.extensions.get_extension_for_class(x509.ExtendedKeyUsage)
+        #     # logger.warning("The extendedKeyUsage extension contains:")
+        #     # logger.warning(extendedKeyUsage)
+        #
+        #     # print cert.serial_number
+        #     # print cert.subject
+        #     # print cert.issuer
+        #     # print cert.signature_hash_algorithm
+        #     # print cert.signature_algorithm_oid
+        #     # print cert.public_key()
+        #     # print cert.extensions
--- checklist.py
+++ checklist.py
@@ -5,6 +5,7 @@ import socket, ssl
 import logging, logging.config, os, pem
 import subprocess
 from cryptography import x509
+from cryptography.x509 import Certificate
 from cryptography.hazmat.backends import default_backend
 from tls_includes import cipher_suites
 import colorlog
@@ -15,15 +16,27 @@ from certificates import *
 hostname='www.de-mail.t-online.de'
 port=443
 
-if which('openssl')==None:
-    logger.error('Could not find openssl in the path. Please install openssl and add it to the path. The call this script again. Will exit now.')
-    exit (1)
+def main():
+    if which('openssl')==None:
+        logger.errori('Could not find openssl in the path. Please install openssl and add it to the path. The call this script again. Will exit now.')
+        exit (1)
 
-# test_supported_protocols(hostname, port)
-# test_supported_cipher_suites(hostname,port)
-# test_session_renegotiation(hostname,port )
-# test_tls_compression(hostname,port)
-# test_heartbeat_extension(hostname,port)
-# TODO 2.4.1 Die verwendeten ephemeren Parameter waￌﾈhrend des TLS-Handshakes bieten ausreichende Sicherheit:
+    # test_supported_protocols(hostname, port)
+    # test_supported_cipher_suites(hostname,port)
+    # test_session_renegotiation(hostname,port )
+    # test_tls_compression(hostname,port)
+    # test_heartbeat_extension(hostname,port)
+    # TODO 2.4.1 Die verwendeten ephemeren Parameter waￌﾈhrend des TLS-Handshakes bieten ausreichende Sicherheit:
 
-certs=read_certificates(hostname,port)
+    certs=read_certificates(hostname,port)
+    # check_certificate_key(certs[0])
+    # check_signature_algorithm(certs[0])
+    # check_for_wildcards(certs[0])
+    # check_cert_for_crl(certs[0])
+    # check_cert_for_aia(certs[0])
+    # check_cert_for_revocation(certs[0])
+    # check_cert_for_keyusage(certs[0])
+    check_cert_for_extended_keyusage(certs[0])
+
+if __name__ == "__main__":
+    main()
